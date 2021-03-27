83379104f677235a6fd6e38320a73f742ea3e0e5
miguelhun <hunmiguel1124@gmail.com>
2021-03-27 17:11:37 -0500

    putting client and listen hashtag together

--- twitter/client.go
+++ twitter/client.go
@@ -1,6 +1,7 @@
 package twitter
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -15,6 +16,25 @@ type Client struct {
 	BaseURL    string
 	apiKey     string
 	HTTPClient *http.Client
+}
+
+type SearchTweetResponse struct {
+	Data []SearchTweet `json:"data"`
+	Meta Metadata
+}
+
+type SearchTweet struct {
+	ID        string `json:"id"`
+	CreatedAt string `json:"created_at"`
+	AuthorID  string `json:"author_id"`
+	Text      string `json:"text"`
+}
+
+type Metadata struct {
+	NewestID    string `json:"newest_id"`
+	OldestID    string `json:"oldest_id"`
+	ResultCount int    `json:"result_count"`
+	NextToken   string `json:"next_token"`
 }
 
 func NewClient() *Client {
@@ -43,4 +63,27 @@ func (c *Client) sendRequest(req *http.Request) ([]byte, error) {
 		return nil, errors.New(fmt.Sprintf("%s", body))
 	}
 	return body, nil
+}
+
+func (c *Client) ListenHashtag() (*SearchTweetResponse, error) {
+	hashtag := config.GetHashtag()
+	hashtagURL := fmt.Sprintf(c.BaseURL+"/tweets/search/recent?query=%s&tweet.fields=author_id,created_at", hashtag)
+
+	req, err := http.NewRequest("GET", hashtagURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.sendRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var response SearchTweetResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
 }
